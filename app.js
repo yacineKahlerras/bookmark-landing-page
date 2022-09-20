ada4d65287088930ae6556b53eb91afe0938b6e1
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-09-20 23:00:11 +0100

    ok

--- app.js
+++ app.js
@@ -24,3 +24,25 @@ sidemenu.addEventListener("click", () => sidemenuVisibility());
  * =========================
  * =========================
  */
+const featureBtns = [...document.querySelector(".features-btns").children];
+const featuresContent = [
+  ...document.querySelector(".features-content-container").children,
+];
+
+const showFeature = (index) => {
+  featuresContent.forEach((feature, featureIndex) => {
+    feature.style.transform = `translate(${index * -100}%, 0)`;
+
+    if (featureIndex == index) feature.style.opacity = `1`;
+    else feature.style.opacity = `0`;
+  });
+
+  featureBtns.forEach((btn, btnIndex) => {
+    if (btnIndex == index) btn.classList.add("feature-selected");
+    else btn.classList.remove("feature-selected");
+  });
+};
+
+featureBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => showFeature(index));
+});
