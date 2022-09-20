f7e1421f90e8176bd8ba95c458b5f0514f3bb292
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-09-20 21:59:15 +0100

    ok

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+/**
+ * =========================
+ * =========================
+ *          sidemenu
+ * =========================
+ * =========================
+ */
+const hamburger = document.getElementById("hamburger");
+const sidemenu = document.querySelector(".sidemenu");
+const nav = document.getElementById("nav");
 
+const sidemenuVisibility = () => {
+  nav.classList.toggle("sidemenu-nav");
+  sidemenu.classList.toggle("show-sidemenu");
+};
+
+hamburger.addEventListener("click", () => sidemenuVisibility());
+sidemenu.addEventListener("click", () => sidemenuVisibility());
+
+/**
+ * =========================
+ * =========================
+ *          features
+ * =========================
+ * =========================
+ */
